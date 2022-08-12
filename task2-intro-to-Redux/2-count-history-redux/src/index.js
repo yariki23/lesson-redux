333531979ee0d77f0bcab546c94d46ddd36726f7
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-08-12 11:05:21 +0300

    new

---
+++ task2-intro-to-Redux/2-count-history-redux/src/index.js
@@ -0,0 +1,41 @@
+import store, { increment, decrement, reset } from './store';
+import './index.scss';
+
+const resultElem = document.querySelector('.counter__result');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+
+const onIncrement = () => {
+  store.dispatch(increment());
+};
+const onDecrement = () => {
+  store.dispatch(decrement());
+};
+const onReset = () => {
+  store.dispatch(reset());
+};
+
+incrementBtn.addEventListener('click', onIncrement);
+decrementBtn.addEventListener('click', onDecrement);
+resetBtn.addEventListener('click', onReset);
+
+const addPlusHistory = history => {
+  let historyStr = [...history];
+  for (let i = 0; i < historyStr.length; i++) {
+    if (historyStr[i + 1] === 1) {
+      historyStr[i + 1] = '+1';
+    }
+  }
+  return historyStr.join('');
+};
+
+store.subscribe(() => {
+  const state = store.getState();
+  const currentValue =
+    state.history.length === 0 ? [] : state.history.reduce((acc, value) => acc + value);
+  const historyString = addPlusHistory(state.history);
+  console.log(historyString);
+  console.log(currentValue);
+  resultElem.textContent = state.history.length === 0 ? '' : `${historyString} = ${currentValue}`;
+});
---
+++ task2-intro-to-Redux/2-count-history-redux/src/store.js
@@ -0,0 +1,51 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+const initialState = {
+  history: [],
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        history: state.history.concat(1),
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        history: state.history.concat(-1),
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
