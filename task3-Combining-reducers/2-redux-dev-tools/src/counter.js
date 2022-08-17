a8d4c8889f2d8096598c91b4511a5e1622117d0f
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-08-17 09:01:55 +0300

    new

--- task3-Combining-reducers/2-redux-dev-tools/src/counter.js
+++ task3-Combining-reducers/2-redux-dev-tools/src/counter.js
@@ -23,9 +23,6 @@ export const counterReducer = (state = 0, action) => {
   }
 };
 
-const store = createStore(
-  counterReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const store = createStore(counterReducer);
 
 export default store;
