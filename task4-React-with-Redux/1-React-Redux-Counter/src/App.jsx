a25f2f5944e26a041adaf04594cce54eb014f562
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-08-18 15:24:20 +0300

    new

--- task4-React-with-Redux/1-React-Redux-Counter/src/App.jsx
+++ task4-React-with-Redux/1-React-Redux-Counter/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Counter from './Counter.jsx';
+import store from './store.js';
 
 const App = () => {
   return (
-    <Provider>
+    <Provider store={store}>
       <Counter />
     </Provider>
   );
---
+++ task4-React-with-Redux/1-React-Redux-Counter/src/store.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux';
+import counterReducer from './counter.reducer';
+
+const store = createStore(counterReducer);
+export default store;
