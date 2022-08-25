08353177b2611f3fb4e82cd1dbf39d14fe859a6c
Yaroslav Shergin <yaroslav.shergin@gmail.com>
2022-08-25 11:28:49 +0300

    new

--- task4-React-with-Redux/3-pagination-react-redux/src/Pagination.jsx
+++ task4-React-with-Redux/3-pagination-react-redux/src/Pagination.jsx
@@ -3,11 +3,8 @@ import React from 'react';
 const Pagination = props => {
   const { currentPage, goPrev, goNext, totalItems, itemsPerPage } = props;
 
-  const isPrevPageAvailable = currentPage === 1;
+  // const isPrevPageAvailable = currentPage === 1;
   const isNextPageAvailable = currentPage === Math.ceil(totalItems / itemsPerPage);
-
-  // const hideLeftArrow = () => ();
-  // const hideRightArrow = () => ();
 
   return (
     <div className="pagination">
